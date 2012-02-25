2f068559e953f7669dd7e42bd251a5876101adee
atwam <wam@atwam.com>
2012-02-25 21:14:21 +0000

    Update rakefile/config.rb for proper root

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/atwam/"
-http_images_path = "/atwam/images"
-http_fonts_path = "/atwam/fonts"
-css_dir = "public/atwam/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
